182a5068f3f3c841618b81db702a0772af67b784
Alan Estevão <allanestevao8@gmail.com>
2020-08-09 19:52:05 -0300

    Finalizando app

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -9,8 +9,20 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 import styles from './styles';
+import api from '../../services/api';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total)
+
+    })
+  }, []);
+  
   const { navigate } = useNavigation();
 
   function handleNavigationToGiveClassesPage() {
@@ -48,7 +60,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões realizadas {' '}
+        Total de {totalConnections} conexões realizadas {' '}
         <Image source={heartIcon} />
       </Text>
     </View>
